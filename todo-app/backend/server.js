a2d5859375659b82883bb64fcd6c55024c78fd53
Adithya Hari V <adithya.hari2022@vitstudent.ac.in>
2025-06-18 01:40:03 +0530

    Update server.js

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://adithyahari2022:Adivarsha04@cluster0.v9bwzfg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
