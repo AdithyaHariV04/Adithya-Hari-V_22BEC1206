481c697d2b655555b371e03441ea6b9db662fe78
AdithyaHariV04 <adithya.hari2022@vitstudent.ac.in>
2025-06-18 00:00:44 +0530

    Added todo-project folder

---
+++ Todo-app/backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const todoRoutes = require('./routes/todoRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('Connection error:', err));
+
+app.use('/api/todos', todoRoutes);
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
